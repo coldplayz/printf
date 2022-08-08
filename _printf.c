b9114fb836afd5ea7d3f8049e279d24e2c3107a0
greenbel <greenbel.chibuike@gmail.com>
2022-08-08 11:52:12 +0100

    Commit 7
    modified:   _printf.c

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@
  */
 int _printf(const char *format, ...)
 {
-	int i, j, ret = 0, ret_count = 0, bytes_written = 0;
+	int i, bytes_written = 0;
 	va_list ap;
 
 	va_start(ap, format);	/*initialize argument pointer*/
@@ -24,76 +24,34 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')	/*when a % character is encountered*/
 		{
-			ret = print_any2(ap, format[++i]);	/*ret stores the number of bytes*/
+			/*ret stores the number of bytes*/
 			/*written by the call to print_any2*/
-			va_end(ap);	/*end the use of ap*/
-			va_start(ap, format);	/*re-initialize ap*/
-			bytes_written += ret;
-			if (ret > 0)
+			switch (format[++i])
 			{
-				if (format[i] != '%')	/*ret_count keeps track of*/
-					/*how many times print_any2*/
-					/*printed a char or string, apart from %*/
-					ret_count += 1;
-				for (j = 0; j < ret_count; j++)	/*reset ap to the correct*/
-					/*position after being used in print_any2*/
-				{
-					va_arg(ap, int);
-				}
-			}
-			else
-			{
-				write(1, (format + i), 1);
-				bytes_written++;
+				case 'c':
+					bytes_written += printc(va_arg(ap, int));
+					break;
+				case 's':
+					bytes_written += prints(va_arg(ap, char *));
+					break;
+				case '%':
+					bytes_written += printp();
+					break;
+				case 'b':
+					bytes_written += printbin(va_arg(ap, unsigned int));
+					break;
+				default:
+					bytes_written += write(1, (format + i), 1);
 			}
 		}
 		else
 		{
-			write(1, (format + i), 1);
-			bytes_written += 1;
+			bytes_written = write(1, (format + i), 1);
 		}
-		ret = 0;
 	}
 	va_end(ap);
 	return (bytes_written);
 }
-
-
-
-/**
- * print_any2 - prints optional arguments based on the format specified by c
- * @ap: an argument pointer
- * @c: character specifying how an optional argument should be printed
- *
- * Return: the number of bytes written/printed to STDOUT
- */
-int print_any2(va_list ap, char c)
-{
-	int bytes_written;
-	char *ptc;
-
-	bytes_written = 0;
-	switch (c)
-	{
-		case 'c':
-			write(1, c2s((va_arg(ap, int)), 0, 0), 1);
-			bytes_written += 1;
-			break;
-		case 's':
-			ptc = va_arg(ap, char *);
-			write(1, ptc, _strlen(ptc));
-			va_arg(ap, char *);
-			bytes_written += _strlen(ptc);
-			break;
-		case '%':
-			write(1, "%", 1);
-			bytes_written += 1;
-			break;
-	}
-
-	return (bytes_written);
-}
-
 
 
 
@@ -147,4 +105,113 @@ int _strlen(char *s)
 	}
 
 	return (len);
+}
+
+
+
+/**
+ * printbin - prints an unsigned int in binary.
+ * @n: the unsigned integer to print.
+ *
+ * Return: the number of characters printed, or -1 if write error.
+ */
+int printbin(unsigned int n)
+{
+	unsigned int i, q = 1, bin_len = 0;
+	int bytes_written = 0, bytes_count = 0;
+	char *ptc;
+
+	q = n;
+	/*find the length of the binary form of the int*/
+	for (i = 0; q != 0; i++)
+	{
+		q = q / 2;
+		bin_len++;
+	}
+	ptc = malloc(sizeof(char) * bin_len + 1);
+	if (ptc == NULL)
+	{
+		write(1, "?", 1);
+		exit(EXIT_FAILURE);
+	}
+
+	q = n;
+	/*populate the ptc's malloc'd array*/
+	/*with digits of binary, in reversed order*/
+	for (i = 0; q != 0; i++)
+	{
+		ptc[i] = (q % 2) + '0';
+		q /= 2;
+	}
+
+	/*print the ptc array, in reverse*/
+	for (i = (bin_len - 1); i >= 0; i--)
+	{
+		bytes_written = write(1, (ptc + i), 1);
+		if (bytes_written == -1)
+			return (-1);
+		bytes_count += bytes_written;
+	}
+	return (bytes_count);
+}
+
+
+/**
+ * printc - prints a char
+ * @arg: the char to print
+ *
+ * Return: the number of bytes written/printed to STDOUT
+ */
+int printc(char arg)
+{
+	int bytes_written;
+
+	bytes_written = 0;
+	write(1, c2s(arg, 0, 0), 1);
+	bytes_written += 1;
+
+	return (bytes_written);
+}
+
+
+/**
+ * printp - prints the percentage character (%).
+ *
+ * Return: the number of bytes written/printed to STDOUT
+ */
+int printp(void)
+{
+	int bytes_written;
+
+	bytes_written = 0;
+
+	bytes_written = write(1, "%", 1);
+
+	if (bytes_written == -1)
+	{
+		return (-1);
+	}
+
+	return (bytes_written);
+}
+
+
+/**
+ * prints - prints the string s
+ * @s: a pointer to the string to print
+ *
+ * Return: the number of bytes written/printed to STDOUT
+ */
+int prints(char *s)
+{
+	int  bytes_written;
+
+	bytes_written = 0;
+	bytes_written = write(1, s, _strlen(s));
+	if (bytes_written == -1)
+	{
+		return (-1);
+	}
+
+	return (bytes_written);
 }
