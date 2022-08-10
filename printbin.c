b174a0e9e5022ca1e342f5ebfb881884565ab5bd
greenbel <greenbel.chibuike@gmail.com>
2022-08-10 07:42:08 +0100

    Commit 46
    modified:   printbin.c

--- printbin.c
+++ printbin.c
@@ -14,8 +14,8 @@
  */
 int printbin(unsigned int n)
 {
-	unsigned int i, q = 1, bin_len = 0;
-	int bytes_written = 0, bytes_count = 0;
+	unsigned int q = 1;
+	int i, bin_len = 0, bytes_written = 0, bytes_count = 0;
 	char *ptc;
 
 	q = n;
