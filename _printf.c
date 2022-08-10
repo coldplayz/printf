b4075393ee9bbaa5bc11d21eebe2bc6b87b591bf
greenbel <greenbel.chibuike@gmail.com>
2022-08-10 17:08:40 +0000

    Commit 50
    modified:   _printf.c - handled the case where the format string is just "%"

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 	int i, j, bytes_count = 0, bytes_written = 0, ret = 0, *pti;
 	va_list ap;
 
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == 0))
 		return (-1);
 	va_start(ap, format);	/*initialize argument pointer*/
 	for (i = 0; format[i]; i++)
